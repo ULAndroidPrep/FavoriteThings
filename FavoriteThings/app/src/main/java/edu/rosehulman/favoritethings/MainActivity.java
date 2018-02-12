a3d2d5aeefd8989c74577c6a2dceef6e300cca58
David Fisher <fisherds@rose-hulman.edu>
2018-02-12 16:06:30 -0500

    finished favorite things example

--- FavoriteThings/app/src/main/java/edu/rosehulman/favoritethings/MainActivity.java
+++ FavoriteThings/app/src/main/java/edu/rosehulman/favoritethings/MainActivity.java
@@ -3,79 +3,116 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView mColorTextView;
-    private TextView mNumberTextView;
-    private long mNumber;
-    private static final String TAG = "FAVES";
+  private TextView mColorTextView;
+  private TextView mNumberTextView;
+  private long mNumber;
+  private static final String TAG = "FAVES";
 
   private static final String COLOR_KEY = "color";
   private static final String NUMBER_KEY = "number";
 
-    // Access a Cloud Firestore instance from your Activity
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-//    private DocumentReference mDocRef = db.collection("favoriteThings").document("Oo0D9GDQ2mUmA7Bbcb9a")
+  // Access a Cloud Firestore instance from your Activity
+  FirebaseFirestore db = FirebaseFirestore.getInstance();
+  //    private DocumentReference mDocRef = db.collection("favoriteThings").document("Oo0D9GDQ2mUmA7Bbcb9a")
   private DocumentReference mDocRef = db.document("favoriteThings/Oo0D9GDQ2mUmA7Bbcb9a");
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mColorTextView = (TextView) findViewById(R.id.color_text_view);
-        mNumberTextView = (TextView) findViewById(R.id.number_text_view);
-        mNumber = 17;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        findViewById(R.id.red_button).setOnClickListener(this);
-        findViewById(R.id.white_button).setOnClickListener(this);
-        findViewById(R.id.blue_button).setOnClickListener(this);
-        findViewById(R.id.update_color_button).setOnClickListener(this);
-        findViewById(R.id.increment_number_button).setOnClickListener(this);
-        findViewById(R.id.decrement_number_button).setOnClickListener(this);
-    }
+    mColorTextView = (TextView) findViewById(R.id.color_text_view);
+    mNumberTextView = (TextView) findViewById(R.id.number_text_view);
+    mNumber = 17;
 
-    @Override
-    public void onClick(View view) {
-      Map<String, Object> dataToSave = new HashMap<>();
-        switch (view.getId()) {
-            case R.id.red_button:
-                mColorTextView.setText(R.string.red);
-                dataToSave.put(COLOR_KEY, "red");
-                break;
-            case R.id.white_button:
-                mColorTextView.setText(R.string.white);
-              dataToSave.put(COLOR_KEY, "white");
+    findViewById(R.id.red_button).setOnClickListener(this);
+    findViewById(R.id.white_button).setOnClickListener(this);
+    findViewById(R.id.blue_button).setOnClickListener(this);
+    findViewById(R.id.update_color_button).setOnClickListener(this);
+    findViewById(R.id.increment_number_button).setOnClickListener(this);
+    findViewById(R.id.decrement_number_button).setOnClickListener(this);
 
-              break;
-            case R.id.blue_button:
-                mColorTextView.setText(R.string.blue);
-              dataToSave.put(COLOR_KEY, "blue");
-
-              break;
-//            case R.id.update_color_button:
-//                Log.d(TAG, "Updating from Firebase");
-//                break;
-            case R.id.increment_number_button:
-                mNumber++;
-                mNumberTextView.setText("" + mNumber);
-              dataToSave.put(NUMBER_KEY, mNumber);
-              break;
-            case R.id.decrement_number_button:
-                mNumber--;
-                mNumberTextView.setText("" + mNumber);
-              dataToSave.put(NUMBER_KEY, mNumber);
-              break;
-
+    mDocRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+      @Override
+      public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+        if (documentSnapshot.exists()) {
+          mNumber = documentSnapshot.getLong("number");
+          mNumberTextView.setText("" + mNumber);
+          String color = documentSnapshot.getString("color");
+          mColorTextView.setText(color);
         }
-        mDocRef.update(dataToSave);
+
+      }
+    });
+  }
+
+  @Override
+  public void onClick(View view) {
+    Map<String, Object> dataToSave = new HashMap<>();
+    switch (view.getId()) {
+      case R.id.red_button:
+//                mColorTextView.setText(R.string.red);
+        dataToSave.put(COLOR_KEY, "red");
+        break;
+      case R.id.white_button:
+//                mColorTextView.setText(R.string.white);
+        dataToSave.put(COLOR_KEY, "white");
+        break;
+      case R.id.blue_button:
+//                mColorTextView.setText(R.string.blue);
+        dataToSave.put(COLOR_KEY, "blue");
+        break;
+      case R.id.update_color_button:
+        Log.d(TAG, "Updating from Firebase");
+
+
+//              mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//
+//                @Override
+//                public void onSuccess(DocumentSnapshot documentSnapshot) {
+//
+//                  if (documentSnapshot.exists()) {
+//                    mNumber = documentSnapshot.getLong("number");
+//                    mNumberTextView.setText("" + mNumber);
+//                    String color = documentSnapshot.getString("color");
+//                    mColorTextView.setText(color);
+//                  }
+//
+//                }
+//              });
+
+
+
+        break;
+      case R.id.increment_number_button:
+        mNumber++;
+//                mNumberTextView.setText("" + mNumber);
+        dataToSave.put(NUMBER_KEY, mNumber);
+        break;
+      case R.id.decrement_number_button:
+        mNumber--;
+//                mNumberTextView.setText("" + mNumber);
+        dataToSave.put(NUMBER_KEY, mNumber);
+
+        break;
+
     }
+    mDocRef.update(dataToSave);
+  }
 }
